f2a1e57bc7eef32e038879f84fd37dfb53ca80e4
yarel2l <yarelleyva2@gmail.com>
2017-02-07 20:22:17 -0500

    Added portfolio app

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
 	url(r'(?P<slug>[a-z0-9-]+)/page/$', views.PortfolioPage.as_view(), name = 'show_info'),
+	url(r'projects/by/(?P<slug>[a-z0-9-]+)/service/$', views.ProjectsListPage.as_view(), name = 'list'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404
 from .models import Project
 from website_1_0.views import Homepage
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 class PortfolioPage(Homepage):
@@ -11,3 +12,24 @@ class PortfolioPage(Homepage):
 		context['page'] = get_object_or_404(Project, slug = slug)
 		context['related_projects'] = Project.objects.filter(project_type__services_projects_related__slug__exact = slug).exclude(slug = slug)
 		return context
+
+
+class ProjectsListPage(Homepage):
+	template_name = 'portfolio/projects.html'
+
+	def get(self, request,  slug = None, **kwargs):
+		projects = Project.objects.filter(project_type__slug = slug)
+		paginator = Paginator(projects, 5)  # Show 5 projects per page
+
+		page = request.GET.get('page')
+		try:
+			projects = paginator.page(page)
+		except PageNotAnInteger:
+			# If page is not an integer, deliver first page.
+			projects = paginator.page(1)
+		except EmptyPage:
+			# If page is out of range (e.g. 9999), deliver last page of results.
+			projects = paginator.page(paginator.num_pages)
+		context = super(ProjectsListPage, self).get_context_data(**kwargs)
+		context['projects'] = projects
+		return render_to_response(self.template_name, context)

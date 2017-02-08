b14bca19218c81ef98c073a680a00d933d6b1b61
yarel2l <yarelleyva2@gmail.com>
2017-02-08 15:55:35 -0500

    Initial structure for configuration app

--- configuration/admin.py
+++ configuration/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.db import models
 from .models import Website, SocialNetworks
 
 
 class SocialNetworkInline(admin.StackedInline):
 	model = SocialNetworks
 	extra = 4
+
 
 class WebsiteAdmin(admin.ModelAdmin):
 	inlines = [SocialNetworkInline]
@@ -22,19 +22,9 @@ class WebsiteAdmin(admin.ModelAdmin):
 			'classes': ('collapse',),
 			'fields': ('header_video_poster', 'header_video'),
 		}),
-		# ('Social Networks', {
-		# 	'classes': ('collapse',),
-		# 	'fields': SocialNetworkInline,
-		# }),
-	)
 
-	# def formfield_for_foreignkey(self, db_field, request, **kwargs):
-	# 	if db_field.name == "car":
-	# 		kwargs["queryset"] = SocialNetworks.objects.filter(website = request.sitename)
-	# 	return super(WebsiteAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+	)
 
 
 admin.site.register(Website, WebsiteAdmin)
-admin.site.register([
-	#SocialNetworks,
-])
+
